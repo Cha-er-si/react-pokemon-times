bf0166baea522864bf2be74f4df2850c65f4882e
Cha-er-si <vpcharles31@gmail.com>
2023-06-23 10:38:00 +0800

    Updated a tag to Link tag of react-router-dom

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export class App extends Component {
         <div className="App">
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
